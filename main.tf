3da35380fce4ba03d8b5aba745cdd21a09ac3d59
Sandesh Shetty <sandeshs@devtools.in>
2021-10-12 18:15:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,6 +32,7 @@ resource "random_pet" "sg" {}
 resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
+  subnet_id     = "subnet-022ae162d2c9c7eea"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
